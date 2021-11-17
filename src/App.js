c8dadca8398127c84d8b35e24219ea42351fd001
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-11-17 09:54:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+import Signup from "./pages/signup/Signup";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Subscription from "./pages/subscription/Subscription";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+
+        <Route path="/subscription/:userId">
+          <Subscription />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
